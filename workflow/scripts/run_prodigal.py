de04f43a9acd1b56004e24aa90327023e53bbd42
Silas Kieser <silas.kieser@gmail.com>
2021-10-14 14:49:04 +0200

    parameter in script need snakemake.

--- workflow/scripts/run_prodigal.py
+++ workflow/scripts/run_prodigal.py
@@ -9,4 +9,4 @@ os.makedirs(snakemake.output[0])
 for path in glob.glob( f"{snakemake.input.fasta_dir}/*.fasta"):
     #I don't understand this rule
     sample_name = "".join(path.split("/")[-1].split(".")[:-1])
-    shell(f"prodigal {snakemake.params.parameters} -i {path} -a {output}/{sample_name}.faa &> {log}")
+    shell(f"prodigal {snakemake.params.parameters} -i {path} -a {snakemake.output}/{sample_name}.faa &> {log}")
