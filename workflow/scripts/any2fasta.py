e8ab85e74bc65cf7815ccb5f97533a5159962aa4
Silas Kieser <silas.kieser@gmail.com>
2021-10-14 14:40:12 +0200

    updated any2fasta script, create prodigal in script so it can use a conda env

--- workflow/scripts/any2fasta.py
+++ workflow/scripts/any2fasta.py
@@ -1,18 +1,23 @@
 import glob
 import os
 import sys
-mkdir_command = f"mkdir {snakemake.output}"
-os.system(mkdir_command)
+from snakemake.shell import shell
+
+
+os.makedirs(snakemake.output[0])
+
 with open(snakemake.input.lists, 'r') as path_file:
     paths = path_file.readlines()
-    for path in paths:
-        path_command = path.strip("\n")
-        unzipped_sample = "".join(path_command.split("/")[-1]).rstrip(".gz")
-        sample_name = ".".join(unzipped_sample.split(".")[:-1])
-        if "gff" in unzipped_sample or ".gz" in path_command:
-            command = f"any2fasta {path_command} > {snakemake.output}/{sample_name}.fasta"
-            os.system(command)
-        else:
-            dst = f"{snakemake.output}/{sample_name}.fasta"
-            command1 = f"ln -s {path_command} {dst}"
-            os.system(command1)
+
+for path in paths:
+    path_command = path.strip("\n")
+    unzipped_sample = "".join(path_command.split("/")[-1]).rstrip(".gz")
+    sample_name = ".".join(unzipped_sample.split(".")[:-1])
+
+    if "gff" in unzipped_sample or ".gz" in path_command:
+        #run any2fasta
+        shell(f"any2fasta {path_command} > {snakemake.output}/{sample_name}.fasta")
+
+    else:
+        # simply create smlink
+        os.symlink(path_command, f"{snakemake.output}/{sample_name}.fasta")
