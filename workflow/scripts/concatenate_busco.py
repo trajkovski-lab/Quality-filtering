51f45c0583595694cd78c347d04d1f48691cc027
Trickovic Matija <trickovi@login1.yggdrasil>
2022-08-03 13:54:51 +0200

    Busco nativly run in batches.

--- workflow/scripts/concatenate_busco.py
+++ workflow/scripts/concatenate_busco.py
@@ -33,8 +33,12 @@ def handle_exception(exc_type, exc_value, exc_traceback):
 sys.excepthook = handle_exception
 
 final_df = pd.DataFrame()
-for table in snakemake.input:
+for input_dir in snakemake.input:
+    table = f"{input_dir}/batch_summary.txt"
     logger.info(f"Found {table} table! Concatenating it to BUSCO_output.tsv . . .")
-    busco_intermediary = pd.read_csv(table, sep="\t", index_col=0)
+    busco_intermediary = pd.read_csv(table, sep="\t").dropna().drop(['Scores_archaea_odb10','Scores_bacteria_odb10','Scores_eukaryota_odb10'],axis=1)
+    busco_intermediary.eval("Completeness = (Complete + 0.5 * Fragmented)/100",inplace=True)
+    busco_intermediary.eval("Contamination = Duplicated / 100",inplace=True)
+    busco_intermediary.eval("Quality_score = Completeness - 5*Contamination - (Fragmented/100)",inplace=True)
     final_df = final_df.append(busco_intermediary, ignore_index=True)
-    final_df.to_csv(snakemake.output[0], sep="\t")
+    final_df.to_csv(snakemake.output[0], sep="\t", index=False)
